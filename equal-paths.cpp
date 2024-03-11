d26c1eb561d9b4724451cadd5de62efc3823fb44
LiAn Song <llsong@usc.edu>
2024-03-11 17:15:28 +0000

    complete equal paths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,5 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
+#include<algorithm>
 
 #endif
 
@@ -8,11 +9,38 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
+// helper function that preserves the longest and shortest paths
+bool equalPathsHelper(Node* root, int len, int& min, int& max);
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+  // Add your code below
+  if (root == nullptr) return true; 
 
+  // declare default values for helper
+  int maxLen = -1;
+  int minLen = -1;    
+  
+  bool pathsAreEqual = equalPathsHelper(root, 0, minLen, maxLen);
+
+  return pathsAreEqual && (maxLen == minLen);
 }
 
+bool equalPathsHelper(Node* root, int len, int& min, int& max) {
+  if (root == nullptr) return true;
+
+  if (root->left == nullptr && root->right == nullptr){
+    if (max == -1 || len > max){
+      max = len;
+    }
+    if (min == -1 || len < min){
+      min = len;
+    }
+    return true;
+  }
+
+  bool left = equalPathsHelper(root->left, len+1, min, max);
+  bool right = equalPathsHelper(root->right, len+1, min, max);
+
+  return left && right;
+}
